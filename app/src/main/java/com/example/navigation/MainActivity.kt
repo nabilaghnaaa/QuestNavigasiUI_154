3f2b7061ba6955257444346200505ccea66fd901
nabilaghnaaa <nabilaghnaaa@gmail.com>
2025-11-07 16:52:37 +0700

    mengganti panggilan di main activity

--- app/src/main/java/com/example/navigation/MainActivity.kt
+++ app/src/main/java/com/example/navigation/MainActivity.kt
@@ -1,4 +1,4 @@
-package com.example.navigation
+package com.example.Navigation
 
 import android.os.Bundle
 import androidx.activity.ComponentActivity
@@ -6,11 +6,14 @@ import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import com.example.Navigation.view.DataApp
 import com.example.navigation.ui.theme.NavigationTheme
 
 class MainActivity : ComponentActivity() {
@@ -19,11 +22,11 @@ class MainActivity : ComponentActivity() {
         enableEdgeToEdge()
         setContent {
             NavigationTheme {
-                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    Greeting(
-                        name = "Android",
-                        modifier = Modifier.padding(innerPadding)
-                    )
+                Surface(
+                    modifier = Modifier.fillMaxSize(),
+                    color = MaterialTheme.colorScheme.background
+                ) {
+                    DataApp()
                 }
             }
         }
