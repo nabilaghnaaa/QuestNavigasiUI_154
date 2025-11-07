2bde6f8f7563af1003e76dfb6def14cbbe1f2765
nabilaghnaaa <nabilaghnaaa@gmail.com>
2025-11-07 10:24:58 +0700

    membuat OutlinedTextField, RadioButton, dan Divider

--- app/src/main/java/com/example/navigation/view/Formulir.kt
+++ app/src/main/java/com/example/navigation/view/Formulir.kt
@@ -8,6 +8,7 @@ import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.res.colorResource
 import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.unit.dp
 import com.example.navigation.R
 
 @OptIn(ExperimentalMaterial3Api::class)
@@ -34,7 +35,51 @@ fun FormIsian(
             verticalArrangement = Arrangement.Top,
             horizontalAlignment = Alignment.CenterHorizontally
         ) {
-            // nanti di commit berikutnya ditambahin form field
+            OutlinedTextField(
+                value = "",
+                onValueChange = {},
+                singleLine = true,
+                label = { Text("Nama Lengkap") },
+                modifier = Modifier
+                    .padding(top = 20.dp)
+                    .width(250.dp)
+            )
+
+            HorizontalDivider(
+                modifier = Modifier
+                    .padding(20.dp)
+                    .width(250.dp),
+                thickness = 1.dp,
+                color = Color.Red
+            )
+
+            Row {
+                jenisK.forEach { item ->
+                    Row(verticalAlignment = Alignment.CenterVertically) {
+                        RadioButton(
+                            selected = false,
+                            onClick = { /* item dipanggil di sini tapi tidak digunakan */ }
+                        )
+                        Text(text = item)
+                    }
+                }
+            }
+
+            HorizontalDivider(
+                modifier = Modifier
+                    .padding(20.dp)
+                    .width(250.dp),
+                thickness = 1.dp,
+                color = Color.Red
+            )
+
+            OutlinedTextField(
+                value = "",
+                onValueChange = {},
+                singleLine = true,
+                label = { Text("Alamat") },
+                modifier = Modifier.width(250.dp)
+            )
         }
     }
 }
